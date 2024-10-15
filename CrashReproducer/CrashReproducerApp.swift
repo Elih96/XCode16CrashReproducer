04571474a19afacac12726b6128db38df7a212a4
Elie Hage <Hage@hey.com>
2024-10-15 10:54:26 +0400

    Initial Commit

---
+++ CrashReproducer/CrashReproducerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrashReproducerApp.swift
+//  CrashReproducer
+//
+//  Created by Elie Hage on 15/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct CrashReproducerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
